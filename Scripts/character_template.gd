0307dd36d0e746a9368ab640ea2046c4a644bb7a
SpookyPlagueDoc777 <mrc.versari@gmail.com>
2026-06-09 17:34:03 +0200

    Bones of the game

---
+++ Scripts/character_template.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
+
+# set amount of double jumps a character can do
+@export var double_jumps = 0
+var jumps_left = double_jumps
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump and double jumps
+	if Input.is_action_just_pressed("jump") and jumps_left != 0:
+		velocity.y = JUMP_VELOCITY
+		jumps_left -= 1
+	
+	# reset jumps_left when you hit floor to designated amount of double jumps
+	if jumps_left < double_jumps and is_on_floor():
+		jumps_left = double_jumps
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
