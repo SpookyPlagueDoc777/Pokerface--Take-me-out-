d90e782ee622fb76d5f1159220250c2121415d4e
McraftYT <epicstudios770@gmail.com>
2026-06-09 22:19:07 +0400

    F11 Support
    Organised and fullscreen

---
+++ Scripts/Globals/global.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var fullscreen: bool = false
+var maximised: bool = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	# Checks if F11 was pressed
+	if Input.is_action_just_pressed("Fullscreen"):
+		if fullscreen == false:
+			if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_MAXIMIZED:
+				maximised = true
+			else:
+				maximised = false
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			fullscreen = true
+				
+		else:
+			if maximised == true:
+				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+			else:
+				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			fullscreen = false
