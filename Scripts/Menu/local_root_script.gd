b639d0aa772fe5ed5ff0071c412785ead0da2231
McraftYT <epicstudios770@gmail.com>
2026-06-14 13:56:27 +0400

    UI Fixes
    I did some stuf

--- Scripts/Menu/local_root_script.gd
+++ Scripts/Menu/local_root_script.gd
@@ -1,12 +1,16 @@
-extends Node2D
+extends Node
 
-@onready var pause_menu: Control = $Pause_menu
+@onready var pause_menu: Control = $CanvasLayer/Pause_menu
+@onready var main: Node2D = $Main
 
 func _ready() -> void:
 	pause_menu.visible = false
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
 		toggle_pause()
 
 func toggle_pause():
