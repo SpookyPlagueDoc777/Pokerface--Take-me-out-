56260292e2c5a1b986c0753471959fafed7f92ba
SpookyPlagueDoc777 <mrc.versari@gmail.com>
2026-06-16 16:57:12 +0200

    MORE settings!!

--- Scripts/Menu/settings_menu.gd
+++ Scripts/Menu/settings_menu.gd
@@ -1,6 +1,9 @@
 extends VBoxContainer
 @onready var origin_menu: CenterContainer = %Pause_menu
 @onready var clicksound: AudioStreamPlayer = $"../../Audio/Clicksound"
+@onready var vhs_shader_effect: ColorRect = $"../../VHS_shader_effect"
+@onready var vh_seffect: CheckButton = $GridContainer/Visuals2/VHSeffect
+
 
 func _on_back_pressed() -> void:
 	%SettingsMenu.visible = false
@@ -17,3 +20,10 @@ func _on_h_sf_xslider_2_value_changed(value: float) -> void:
 	
 func _on_hmusicslider_3_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), value)
+
+
+func _on_vh_seffect_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		vhs_shader_effect.visible = true
+	if toggled_on == false:
+		vhs_shader_effect.visible = false
